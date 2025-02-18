3a6fc5e2916f7570ad79a800a4fbff5efd1d4174
AmodKatiyar <amod@post105.com>
2025-02-18 14:55:06 +0530

    Added plus icon

--- src/components/Body/Pins.jsx
+++ src/components/Body/Pins.jsx
@@ -2,9 +2,27 @@ import React, { useEffect, useState } from "react";
 import Masonry from "masonry-layout";
 import imagesLoaded from "imagesloaded";
 import axios from "axios";
+import cross from '../../assets/cross.svg'
 
 const Pins = ({ selectedCategory, selectedMaterial }) => {
-  const [itemData, setItemData] = useState([]);
+  const [itemData, setItemData] = useState([])
+
+  // const demo=[{
+  //   img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+  //   title: 'Fern',
+  // },
+  // {
+  //   img: 'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
+  //   title: 'Snacks',
+  // },
+  // {
+  //   img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+  //   title: 'Mushrooms',
+  // },
+  // {
+  //   img: 'https://images.unsplash.com/photo-1529655683826-aba9b3e77383',
+  //   title: 'Tower',
+  // }]
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -46,9 +64,12 @@ const Pins = ({ selectedCategory, selectedMaterial }) => {
     <div className="pins">
       <div className="image-grid">
         {itemData.map((item, index) => (
-          <div className="image-item" key={index} onClick={() => handleImageClick(item.title)}>
+          <div className="image-item" key={index}>
             <div className="image-wrapper">
               <img src={item.img} alt={item.title} className="cursor-pointer" />
+              <div className="plusIcon">
+              <img src={cross} alt="Click" onClick={() => handleImageClick(item.title)}/>
+              </div>
             </div>
           </div>
         ))}
